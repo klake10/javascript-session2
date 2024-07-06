727c6557b2c125acadaf559f32fdaa21a7c28dd9
Krystal Lake <krystal.lake10@gmail.com>
2024-07-06 21:56:16 +0100

    if else prompt example

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 console.log("Hello")
+
+let price = 100
+const discount = 20
+
+console.log("Price " + price)
+
+//Apply discount
+price = price * ((100 - discount) /100)
+
+console.log("New Price: " + price)
+
+
+
+const response = prompt("Books vs Movies")
+if (response === "books") {
+        console.log("Books")
+} else {
+    console.log("Films")
+}
